f6d1cdc12ac14e6065aef02a6a2ace3a08ba2521
Elias Frantar <elias.frantar@gmail.com>
2019-12-01 13:42:53 +0100

    Added very simple timer GUI

---
+++ gui.py
@@ -0,0 +1,49 @@
+import time
+from tkinter import *
+from threading import Thread
+
+class TimerGUI:
+    
+    def __init__(self):
+        self.gui = Tk()
+        self.gui.title('mirrcub3r')
+
+        self.timel = Label(self.gui, text='seconds', font=('Mono Bold', 150), name='time')
+        self.timel.config(anchor=CENTER)
+        self.timel.pack()
+
+        self.mirrcub3rl = Label(self.gui, text='mirrcub3r', font=('Arial', 100))
+        self.mirrcub3rl.config(anchor=CENTER)
+        self.mirrcub3rl.pack()
+
+        self.credits = Label(self.gui, text='by Elias Frantar', font=('Arial', 50))
+        self.credits.config(anchor=CENTER)
+        self.credits.pack()
+
+        self.reset()
+        
+    def show(self, parallel):
+        self.gui.after(0, lambda: Thread(target=parallel).start())
+        self.gui.mainloop()
+
+    def reset(self):
+        self.running = False
+        self.thread = Thread(target=self.run)
+        self.update(0)
+
+    def update(self, time):
+        self.timel.configure(text='%.3f seconds' % time)
+
+    def start(self):
+        self.running = True
+        self.thread.start()
+
+    def stop(self):
+        self.running = False
+
+    def run(self):
+        start = time.time()
+        while self.running:
+            time.sleep(.001)
+            self.update(time.time() - start)
+
--- main.py
+++ main.py
@@ -11,63 +11,72 @@ from threading import Thread
 import numpy as np
 
 from control import *
+from gui import *
 from scan import *
 from solve import *
 
-config = ConfigParser()
-config.read('config')
-config = config['DEFAULT']
+def main():
+    config = ConfigParser()
+    config.read('config')
+    config = config['DEFAULT']
 
-with Solver() as solver:
-    print('Solver initialized.')
+    with Solver() as solver:
+        print('Solver initialized.')
 
-    robot = Robot()
-    print('Connected to robot.')
+        robot = Robot()
+        print('Connected to robot.')
 
-    points = np.array(pickle.load(open(config['pos'], 'rb')))
-    extractor = ColorExtractor(points, int(config['scan_size']))
-    matcher = ColorMatcher()
-    cam = IpCam(config['cam'])
-    print('Scanning set up.')
+        points = np.array(pickle.load(open(config['pos'], 'rb')))
+        extractor = ColorExtractor(points, int(config['scan_size']))
+        matcher = ColorMatcher()
+        cam = IpCam(config['cam'])
+        print('Scanning set up.')
 
-    print('Ready.') # we don't want to print this again and again while waiting for button presses
-    while True: # polling is the most straight-forward way to check both buttons at once
-        time.sleep(.05) # 50ms should be sufficient for a smooth experience
-        if robot.scramble_pressed():
-            scramble = solver.scramble()
+        print('Ready.') # we don't want to print this again and again while waiting for button presses
+        while True: # polling is the most straight-forward way to check both buttons at once
+            time.sleep(.05) # 50ms should be sufficient for a smooth experience
+            if robot.scramble_pressed():
+                gui.reset()
+                scramble = solver.scramble()
+                start = time.time()
+                robot.execute(scramble)
+                print('Scrambled! %fs' % (time.time() - start))
+                continue
+            elif not robot.solve_pressed():
+                continue
+            # Now actually start solving
+
+            cam.flash(True)
+            frame = cam.frame()
+            # NOTE: We start timing only after we have received a frame from the camera and start any processing.
+            # While this might not be 100% conform to the Guiness World Record rules, I am (at least at this point)
+            # not interested in optimizing the camera latency as I do not think this should be an integral part
+            # of a cube-solving robot.
+            gui.reset()
+            gui.start()
             start = time.time()
-            robot.execute(scramble)
-            print('Scrambled! %fs' % (time.time() - start))
-            continue
-        elif not robot.solve_pressed():
-            continue
-        # Now actually start solving
+            print('Scanning ...')
+            scans = extractor.extract_bgrs(frame)
+            facecube = matcher.match(scans)
+            print('Solving ...')
+            sol = solver.solve(facecube)
+            print(time.time() - start)
 
-        cam.flash(True)
-        frame = cam.frame()
-        # NOTE: We start timing only after we have received a frame from the camera and start any processing.
-        # While this might not be 100% conform to the Guiness World Record rules, I am (at least at this point)
-        # not interested in optimizing the camera latency as I do not think this should be an integral part
-        # of a cube-solving robot.
-        start = time.time()
-        print('Scanning ...')
-        scans = extractor.extract_bgrs(frame)
-        facecube = matcher.match(scans)
-        print('Solving ...')
-        sol = solver.solve(facecube)
-        print(time.time() - start)
+            # Turn off flash only after processor is not busy with solving anymore
+            flash_off = Thread(target=lambda: cam.flash(False)) 
+            flash_off.start()
 
-        # Turn off flash only after processor is not busy with solving anymore
-        flash_off = Thread(target=lambda: cam.flash(False)) 
-        flash_off.start()
+            if sol is not None:
+                print('Executing ...')
+                robot.execute(sol)
+                print('Solved! %fs' % (time.time() - start))
+            else:
+                print('Error.')
+            gui.stop()
+            
+            flash_off.join()
+            print('Ready.')
 
-        if sol is not None:
-            print('Executing ...')
-            robot.execute(sol)
-            print('Solved! %fs' % (time.time() - start))
-        else:
-            print('Error.')
-        
-        flash_off.join()
-        print('Ready.')
+gui = TimerGUI()
+gui.show(main)
 
