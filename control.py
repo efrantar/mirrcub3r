f8e60029a667e3a9c2e123cdad43f6285c0281a7
Elias Frantar <elias.frantar@gmail.com>
2020-01-01 17:07:23 +0100

    Added one more thing to .gitignore

--- control.py
+++ control.py
@@ -112,7 +112,7 @@ def optim_halfdirs(sol):
     return sol1
 
 
-# SAFETY = 1.25 # noticeably slower + but quite a bit more reliable
+# SAFETY = 1.25 # noticeably slower + but also a lot more reliable
 SAFETY = 1.
 
 # [][0] for quarter- and [][1] for half-turns
